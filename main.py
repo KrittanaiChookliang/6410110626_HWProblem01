9e26416077b94b63f6f8b02cd4890f45bcce03e3
กฤตนัย ชูเกลี้ยง <krittanai.st@gmail.com>
2024-07-22 00:25:17 +0700

    read file from json

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ import pandas as pd
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import json
+
+# ตรวจสอบเส้นทางไปยังไฟล์ JSON
+file_path = 'pp3-4_2566_province.json'
+geojson_path = 'thailand-provinces.json'  # เพิ่มเส้นทางไปยังไฟล์ GeoJSON
+
+# อ่านข้อมูลจากไฟล์ JSON
+df = pd.read_json(file_path)
